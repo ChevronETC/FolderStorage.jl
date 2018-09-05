0084750871171760a52172ad19a77b64753fe187
samtkaplan <sam.kaplan@chevron.com>
2018-09-05 07:25:27 -0500

    only run build script if not windows.

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,11 @@
-try
-    rm("usr/lib",recursive=true,force=true)
-catch
-    warn("problem removing $pwd/usr/lib")
+if !Sys.iswindows()
+    try
+        rm("usr/lib",recursive=true,force=true)
+    catch
+        warn("problem removing $pwd/usr/lib")
+    end
+    mkpath("usr/lib")
+    cd("../src")
+    run(`make`)
+    run(`make clean`)
 end
-mkpath("usr/lib")
-cd("../src")
-run(`make`)
-run(`make clean`)
